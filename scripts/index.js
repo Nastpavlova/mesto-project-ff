d758b5eca8cee902b9cba2b48107f11d28ae512b
Nastpavlova <115315122+Nastpavlova@users.noreply.github.com>
2024-12-25 17:07:06 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const placeList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
+function createCard(item, deleteCardItem) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+    cardTitle.textContent = item.name;
+    cardDeleteButton.addEventListener('click', function() {
+        deleteCard(cardElement);
+    });
+    return cardElement;
+}
 
 // @todo: Вывести карточки на страницу
+function displayCards(initialCardsFull, deleteCard) {
+    for (const item of initialCardsFull) {
+        const newCardElement = createCard(item, deleteCard);
+        placeList.append(newCardElement);
+    }
+}
+
+// @todo: Функция удаления карточки
+function deleteCard(element) {
+    element.remove();
+}
+
+//вызываем функцию
+displayCards(initialCards, deleteCard);
